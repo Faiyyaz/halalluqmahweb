f56e10ac745fe716c7a393db42d871cdcb842510
Mohammed Faiyyaz Khatri <kfaiyyaz93@gmail.com>
2026-01-02 21:12:00 +0530

    search

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,3 +1,36 @@
+import useRestaurants from "@/hooks/useRestaurants";
+import { useEffect, useState } from "react";
+
 export default function Home() {
-  return <label>Halal Luqmah</label>;
+  const [searchTerm, setSearchTerm] = useState<string | null>(null);
+  const [shouldSearch, setShouldSearch] = useState(false);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  const { restaurants, loading, error } = useRestaurants({
+    searchTerm,
+    shouldSearch,
+    setShouldSearch,
+    currentPage,
+    onError: () => {
+      setShouldSearch(false);
+      if (currentPage > 1) {
+        setCurrentPage(currentPage - 1);
+      }
+    },
+  });
+
+  return (
+    <div className="flex flex-col">
+      {restaurants.map((restaurant) => (
+        <div key={restaurant.$id}>{restaurant.name}</div>
+      ))}
+      <input onChange={(e) => setSearchTerm(e.target.value)} />
+      <button
+        disabled={!searchTerm || searchTerm?.trim() === ""}
+        onClick={() => setShouldSearch(true)}
+      >
+        Search
+      </button>
+    </div>
+  );
 }
